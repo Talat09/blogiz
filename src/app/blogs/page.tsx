a9150d2fce6ad7bd1015abc07e4f5c7d3e481863
Talat09 <talat181008312@gmail.com>
2025-05-29 01:08:16 +0600

    done:blogs data fetching using RTK query

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,10 +1,14 @@
+"use client";
 import BlogCard from "@/components/ui/BlogCard";
+import { useGetBlogsQuery } from "@/redux/api/baseApi";
 import { Blog } from "@/type";
 import React from "react";
 
-const BlogsPage = async () => {
-  const res = await fetch("http://localhost:5000/blogs", { cache: "no-store" });
-  const blogs = await res.json();
+const BlogsPage = () => {
+  //   const res = await fetch("http://localhost:5000/blogs", { cache: "no-store" });
+  //   const blogs = await res.json();
+  const { data: blogs, isLoading, isError, error } = useGetBlogsQuery("");
+  console.log("BlogsPage data:", blogs);
   return (
     <div className="w-[90%]  mx-auto my-10 p-4   md:p-10">
       <h1 className="text-4xl text-center my-5 font-semibold">
@@ -18,7 +22,7 @@ const BlogsPage = async () => {
         </i>
       </p>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-8 mt-10 justify-items-center  ">
-        {blogs.map((blog: Blog) => (
+        {blogs?.map((blog: Blog) => (
           <BlogCard key={blog.id} blog={blog} />
         ))}
       </div>
